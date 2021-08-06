9c111ef0c421226913f091bbd73b41f72484182c
金楠 <1299973527@qq.com>
2021-08-06 10:35:25 +0800

    '图片验证码设置'

--- small/users/urls.py
+++ small/users/urls.py
@@ -1,10 +1,13 @@
 # 用于users子应用的视图路由
 from django.urls import path
 from users.views import RegisterView
+from users.views import ImageCodeView
 
 urlpatterns = [
     # path的第一个参数1：路由
     # 参数2：视图函数
     # 参数3：路由名，方便通过reverse来获取路由
     path('register/',RegisterView.as_view(),name='register'),
+    # 图片验证码的路由
+    path('imagecode/',ImageCodeView.as_view(),name='imagecode')
 ]
--- small/users/views.py
+++ small/users/views.py
@@ -14,3 +14,32 @@ class RegisterView(View):
         :return: 注册界面
         """
         return render(request, 'register.html')
+
+
+from django.http.response import HttpResponseBadRequest
+from libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
+from django.http import HttpResponse
+
+class ImageCodeView(View):
+
+    def get(self, request):
+
+        # 1.接收前端传递过来的uuid
+        uuid=request.GET.get('uuid')
+        # 2.判断uuid是否获取到
+        if uuid is None:
+            return HttpResponseBadRequest('没有传递uuid')
+        # 3.通过调用captcha来生成图片验证码（图片二进制和图片内容）
+        text,image=captcha.generate_captcha()
+        # 4.将图片内容保存在redis中 uuid作为一个key,图片内容作为value，同时我们可以设置一个时效
+        redis_conn=get_redis_connection('default')
+        # key设置为uuid
+        # seconds设置过期秒数，设为300秒，即5分钟过期时间
+        # value 设置为text
+        redis_conn.setex('img:%s'%uuid,300,text)
+        # 5.返回图片二进制
+        return HttpResponse(image,content_type='image/jpeg')
+
+
+
